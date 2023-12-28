64521573e7d5bb7d4baaf28e28eddc36ca9b05ff
SimoCPZ <simonecapezzani@gmail.com>
2023-12-28 12:13:38 +0100

    Add simple players tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import Player from "./components/Player";
+
 
 function App() {
 
 
-  return (
-    <p>Coming soon...</p>
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player name="player 1" symbol="X" />
+        <Player name="player 2" symbol="O" />
+      </ol>
 
-  )
+      GAME BOARD
+    </div>
+  </main>;
 }
 
 export default App
